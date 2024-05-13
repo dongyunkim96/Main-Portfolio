9582086b7ab386ccf431cc89bb63beb78b25dabc
dongyunkim96 <dongyunkim96@gmail.com>
2024-05-13 15:16:42 -0400

    resume part add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate
+  Navigate,
   } from "react-router-dom";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
@@ -11,6 +11,8 @@ import Footer from "./components/Footer";
 import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
+import ScrollToTop from "./components/ScrollToTop";
+import Resume from "./components/Resume/Resume";
 import './App.css';
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -31,10 +33,13 @@ function App() {
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
       <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/project" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
       </div>
