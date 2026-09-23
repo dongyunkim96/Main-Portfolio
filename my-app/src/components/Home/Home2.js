e7fce9b4b4f975458272c2891ee56eafb0fa5efc
dongyunkim96 <dongyunkim96@gmail.com>
2026-09-23 15:55:25 -0400

    profile img upload

--- my-app/src/components/Home/Home2.js
+++ my-app/src/components/Home/Home2.js
@@ -56,7 +56,7 @@ function Home2() {
                     <Col md={12} className="home-about-social">
                         <h1>FIND ME ON</h1>
                         <p>
-                            Feel Free To <span className="purple">Connect</span>With Me
+                            Feel Free To <span className="purple">Connect</span> With Me
                         </p>
                         <ul className="home-about-social-links">
                             <li className="social-icons">
